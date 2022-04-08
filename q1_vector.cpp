f3e20a3638ec645b29154093a44f1e21a4d5559a
triyasha <98093193+triyasha@users.noreply.github.com>
2022-04-08 21:18:13 +0530

    Add files via upload

---
+++ q1_vector.cpp
@@ -0,0 +1,68 @@
+/*
+Q1: Vectors: Write a C++ program, there you take two vectors of names. The program should return a
+vector that has all the names only once.
+Example: vector1 = [Virendra Sachin, Saurav, Rahul, Laxman]
+vector2 = [Harbhajan, Ashish, Rahul, Yuvraj, Saurav]
+output = [Virendra Sachin, Saurav, Rahul, Laxman, Harbhajan, Ashish, Yuvraj]
+*/
+
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int m;
+    vector<string>v1(m);
+    cout << "enter the total value you want for 1st vector : " << endl;
+    cin >> m;
+    cout << "enter value for vector 1" << endl;
+    for(int i=0;i<m-1;i++)
+    {
+        cin >> v1[i];
+    }
+    cout << "vector 1 is : " << endl;
+    for(int i=0;i<m-1;i++)
+    {
+        cout << v1[i] << " ";
+    }
+    cout << endl;
+
+    int n;
+    vector<string>v2(n);
+    cout << "enter the total value you want for 2st vector : " << endl;
+    cin >> n;
+    cout << "enter value for vector 2" << endl;
+    for(int i=0;i<n-1;i++)
+    {
+        cin >> v2[i];
+    }
+    cout << "vector 2 is : " << endl;
+    for(int i=0;i<n-1;i++)
+    {
+        cout << v2[i] << " ";
+    }
+    cout << endl;
+
+	int p;
+    vector<string> v3(p);
+	for(int i=0;i<p-1;i++)
+	{
+	    if(v1[i] == v2[i])
+	    {
+	        v3[i] += v1[i];
+	    }
+	    else if(v1[i] != v2[i])
+	    {
+	        v3[i] += v2[i];
+	    }
+	}
+	cout<<"\nFinal vector is : \n";
+	for(int i=0;i<p-1;i++)
+	{
+		cout<<v3[i]<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
